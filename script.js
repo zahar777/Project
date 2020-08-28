bc1db8b956d194abb8024712d02945e426622b34
Zahar <zahar888ri@gmail.com>
2020-08-28 17:26:42 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+"use strict";
+
+console.log(4 + 5);
+
+let incr =10,
+decr =10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2+2*4==10);
+ 
+
+
+const   isChecked = false,
+        isClose=false;
+
+console.log(isChecked || !isClose);
+ 
+alert("hello");
